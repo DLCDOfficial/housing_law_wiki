6cc984f655d195833716992eb72bb8d024c4c050
DLCDCDO <joshua.tanner@dlcd.oregon.gov>
2026-03-05 11:14:16 -0800

    chore: build

---
+++ dist/assets/readme-Czc21-Nr.js
@@ -0,0 +1,35 @@
+const e=`# Housing Law
+
+This field is the category or topic of housing law. My thinking is we utilize the same categories in our Smartsheet so there is parity between the two.
+
+# Short Description
+
+This will be a very brief (~140 character) description of the housing law, similar to our current guidance doc but adapted to fit on the cards for each topic.
+
+# Long Description
+
+This will be a longer description of the law accessed by clicking on the card. In terms of depth/detail, these will be comparable to the summaries of legislation in the 2025 Session Guidance doc, but adapted to describe the law as it is today (not how it has changed). The mandatory adjustment summary is also a comparable summary in terms of length/formatting.
+
+# Housing Project Examples
+
+This isn’t so much a field as it is a database where we can provide project examples that make use of a given law (or laws). We would enter the project name, description of the project (and how it used the relevant law), any associated images, and a housing law category that is filterable, so they show up on the right page.
+
+Note that this component is not critical for launch. We could build it after launch.
+
+# Related Materials
+
+This will link to any relevant materials we or others produce on housing laws. Examples include one-pagers, graphical aids, model codes, webinar recordings, etc.
+
+# Related Statutes/Laws, Bills, and Administrative Rules
+
+This will be a list of and links to relevant statutes, bills, and administrative rules. A few questions for how we structure this one:
+
+1. Should we include a brief 140-character description for each item? For example, a bill summary describing briefly how it affected the law.
+
+2. We could break this out into each component (i.e. statute/law is separate from bills is separate from rules), which is cleaner but might take more real estate
+
+3. I do not think we ought to cite case law at this time given that we lack the means to do the research and cannot offer legal advice to cities. Open to revisiting in the future if we identify important case law that we think needs to be external.
+
+# Question and Answer
+
+This is less a distinct field and more of a database for all of the questions and answers we receive for commonly asked questions. This should be searchable and filterable by housing law topic. Under each card, we should have a window containing the Q/A already set to the right housing law filter to show questions for that particular law.`;export{e as default};
